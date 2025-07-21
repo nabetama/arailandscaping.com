3a4f82d8a0635800fb1eb4611c7452b1684c21a5
nabetama <mao.nabeta@gmail.com>
2025-07-21 14:43:04 +0900

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
+export default defineConfig(({ command, mode }) => ({
   plugins: [react()],
   build: {
-    outDir: '../dist',
+    outDir: 'dist',
     assetsDir: 'assets',
   },
   server: {
     port: 3000,
     open: true
   },
-  publicDir: '../public',
-  base: '/',
-  root: 'src'
-}) 
+  preview: {
+    port: 4173,
+    open: true
+  },
+  publicDir: 'public',
+  base: mode === 'production' ? './' : '/',
+  root: '.'
+})) 
