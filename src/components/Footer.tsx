2f35b926454eb1f8d96dd587a8788530fd736456
nabetama <mao.nabeta@gmail.com>
2026-01-30 19:00:59 +0900

    Update copyright text in Footer component

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ const Footer: React.FC = () => {
   return (
     <footer className="flex items-center justify-center h-15 bg-bg-black text-text-hanshi text-center text-sm tracking-wide py-3">
       <p className="text-text-hanshi text-xs">
-        &copy; 2024 アライランドスケーピング株式会社 All Rights Reserved.
+        &copy; 2024 株式会社アライランドスケーピング All Rights Reserved.
       </p>
     </footer>
   )
